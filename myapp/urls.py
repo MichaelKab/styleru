1f9fdbaa561919845e14420602c8200da944ed33
Michael <mihcael.kabanov4@gmail.com>
2023-11-08 19:09:44 +0300

    nata base

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+app_name = 'my_app'
+router = DefaultRouter()
+router.register('items', views.ItemViewSet, basename="item-viewset")
+
 urlpatterns = [
-    path('<int:start>/<int:end>', views.index, name='index'),
     path('', views.index, name='index'),
+    # path('items/', views.ItemAPIView.as_view(), name='item-view'),
 ]
+urlpatterns += [path(r'api/', include(router.urls))]
